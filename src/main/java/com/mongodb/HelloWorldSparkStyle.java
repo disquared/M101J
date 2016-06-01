1fd866c06d606bbbda391e62bf32e8a6433a89ca
Di Di <ddi0168@gmail.com>
2016-05-31 21:30:39 -0400

    Week 1 files

---
+++ src/main/java/com/mongodb/HelloWorldSparkStyle.java
@@ -0,0 +1,18 @@
+package com.mongodb;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class HelloWorldSparkStyle {
+
+    public static void main(String[] args) {
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World from Spark";
+            }
+        });
+    }
+}
